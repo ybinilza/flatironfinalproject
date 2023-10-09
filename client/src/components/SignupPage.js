27b5945c1b7850b8843e0b5744422d10bee63913
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-09 19:53:16 -0400

    updates SignupPage

--- client/src/components/SignupPage.js
+++ client/src/components/SignupPage.js
@@ -1,20 +1,55 @@
 import React from 'react'
 import './SignupPage.css'
+import { useState } from 'react';
 
 function SignupPage() {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] =useState("");
+  const [password, setPassword] = useState("");
+
+/*
+ cnsole.log(username)
+ console.log(email)
+ console.log(password)
+*/
+
+function handleSubmit(e)
+{
+  e.preventDefault();
+  console.log(username)
+  console.log(email)
+  console.log(password)
+  
+}
+
   return (
     <div>
       <div className="signup-container">
-        <form action="#" method="post">
+        <form onSubmit={handleSubmit}>
           <h2>Sign Up</h2>
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
+          <input type="text"
+                id="username" 
+                name="username" required
+                autoComplete="off"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                 />
 
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          <input type="email" 
+                  id="email" 
+                  name="email" required
+                  value={email}
+                  onChange={(e) =>setEmail(e.target.value)} />
 
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <input type="password" 
+                 id="password" 
+                 name="password"
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)} />
 
           <input type="submit" value="Sign Up" />
         </form>
