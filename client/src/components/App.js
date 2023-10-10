c9968c3f42638f7278027c8e27206cfe7eae2528
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-10 15:15:35 -0400

    updated react components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import SignupPage from "./SignupPage";
 import LoginPage from "./LoginPage";
+import Navbar from "./Navbar"
 
 function App() {
   return(<div>
     <h1>
-      <LoginPage></LoginPage>
+      <Navbar></Navbar>
     </h1>
   </div>)
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,23 +5,19 @@ function Navbar() {
     return (
         <nav className="navbar">
           <div className="logo-container">
-            <img src="path-to-your-logo.png" alt="Logo" className="logo" />
+            <img  alt="Logo" className="logo" />
           </div>
           <div className="search-container">
             <input type="text" placeholder="Search" className="search-bar" />
           </div>
           <div className="buttons-container">
-            {isLoggedIn ? (
-              <button onClick={handleLogout}>Logout</button>
-            ) : (
-              <button onClick={handleLogin}>Login</button>
-            )}
+            
+              <button >Logout</button>
+            
+              <button >Login</button>
+            
           </div>
         </nav>
       );
-
+}
 export default Navbar
-
-
-
-
