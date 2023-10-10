c089de120243026f05fac2b0dddf9db9a7a4d559
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-09 23:02:27 -0400

    Add yup and formik

--- client/src/components/SignupPage.js
+++ client/src/components/SignupPage.js
@@ -1,87 +1,98 @@
-import React from 'react'
-import './SignupPage.css'
-import { useState } from 'react';
+import React from 'react';
+import './SignupPage.css';
+import { useFormik } from 'formik';
+import * as yup from "yup";
 
 function SignupPage({ onLogin }) {
-
-  const [username, setUsername] = useState("");
-  const [email, setEmail] =useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-
-function handleSubmit(e)
-{
-  e.preventDefault();
-  setIsLoading(true);
-  /*console.log(username)
-  console.log(email)
-  console.log(password)*/
-
-  fetch("/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      email,
-      password,
-    }),
-  }).then((r) => {
-    setIsLoading(false);
-    if (r.ok) {
-      r.json().then((user) => {
-        onLogin(user);
-        setErrors([]);   // Reset errors when the form is submitted successfully
-      });
-    } else {
-      r.json().then((err) => setErrors(err.errors || []));
-    }
+  
+  const formSchema = yup.object().shape({
+    username: yup.string().required("Must enter a username").max(15),
+    email: yup.string().email("Invalid email").required("Must enter an email"),
+    password: yup.string()
+      .min(8, 'Password must be at least 8 characters')
+      .required('Password is required'),
   });
-}
 
-console.log(errors)
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: formSchema,
+    onSubmit: async (values) => {
+      try {
+        const response = await fetch("/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
+
+        if (response.ok) {
+          const user = await response.json();
+          onLogin(user);
+        } else {
+          const errorData = await response.json();
+          formik.setErrors(errorData.errors || {});
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
+    },
+  });
 
   return (
     <div>
       <div className="signup-container">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
           <h2>Sign Up</h2>
           <label htmlFor="username">Username:</label>
-          <input type="text"
-                id="username" 
-                name="username" required
-                autoComplete="off"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                 />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            autoComplete="off"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+          />
+          <div>{formik.errors.username}</div>
 
           <label htmlFor="email">Email:</label>
-          <input type="email" 
-                  id="email" 
-                  name="email" required
-                  value={email}
-                  onChange={(e) =>setEmail(e.target.value)} />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          <div>{formik.errors.email}</div>
 
           <label htmlFor="password">Password:</label>
-          <input type="password" 
-                 id="password" 
-                 name="password"
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+          <div>{formik.errors.password}</div>
 
-          <input type="submit" value="Sign Up" />
+          <button type="submit" disabled={formik.isSubmitting}>
+            Sign Up
+          </button>
         </form>
       </div>
-      {isLoading && <p>Loading...</p>}
-      {errors.length > 0 && (
+      {formik.isSubmitting && <p>Loading...</p>}
+      {Object.keys(formik.errors).length > 0 && (
         <div>
           <p>Error:</p>
           <ul>
-            {errors.map((error, index) => (
-              <li key={index}>{error}</li>
+            {Object.keys(formik.errors).map((fieldName, index) => (
+              <li key={index}>{formik.errors[fieldName]}</li>
             ))}
           </ul>
         </div>
@@ -89,4 +100,5 @@ console.log(errors)
     </div>
   );
 }
-export default SignupPage
+
+export default SignupPage;
