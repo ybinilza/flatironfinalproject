3243287fadb0cfc5c0321c89be4765fec5c0d6ea
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-09 23:23:30 -0400

    Added user table,bcrypt

--- server/app.py
+++ server/app.py
@@ -5,6 +5,17 @@
 # Remote library imports
 from flask import request
 from flask_restful import Resource
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask import request, session, jsonify, make_response
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from config import app, db, api
+from models import User
 
 # Local imports
 from config import app, db, api
@@ -16,6 +27,36 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+        username = json.get("values.username")
+        email=json.get("values.email")
+        password = json.get("values.password")
+        
+        user = User(
+            username=username,
+            email=email,
+            password_hash=password,
+        )
+
+        if user.validate_username(username, user.username) and user.validate_email(username, user.email) :
+            db.session.add(user)
+            db.session.commit()
+
+            session["user_id"] = user.id
+
+            response = make_response(
+                user.to_dict(),
+                201,
+            )
+
+            return response
+        else:
+            return {}, 422
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,6 +1,46 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from config import db, bcrypt
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.exc import IntegrityError
+from config import db, bcrypt
 
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email=db.Column(db.String,nullable=False)
+    _password_hash = db.Column(db.String)
+
+
+    def __repr__(self):
+        return f"User {self.username}, ID: {self.id}"
+    
+    @property
+    def password_hash(self):
+        raise AttributeError("Password is not accessible")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+    
+    @validates("username")
+    def validate_username(self, key, value):
+        if value == "":
+            raise ValueError("Username is required")
+        return value
+    
+    @validates("email")
+    def validate_email(self, key, value):
+        if value == "":
+            raise ValueError("email is required")
+        return value
+
